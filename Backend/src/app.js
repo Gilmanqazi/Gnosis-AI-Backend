c1594f8a69786c94ea43bd8615a3bef3f7474d8b
gilman qazi <gilmanqazi@gmail.com>
2026-03-21 19:09:26 +0530

    fix: update cors origin with array

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set("trust proxy", 1);
 
 // --- 🌐 MIDDLEWARE ---
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
